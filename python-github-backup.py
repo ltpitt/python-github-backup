c530de988a985f73b6e03cf25e68bcbdb5de0226
Davide Nastri <d.nastri@gmail.com>
2017-03-25 00:34:05 +0100

    feat: Added folder retention, refactored and commented code

--- python-github-backup.py
+++ python-github-backup.py
@@ -2,24 +2,62 @@ import requests
 import json
 import subprocess
 import os
+import datetime
+import shutil
 
 GITHUB_USERNAME = "ltpitt"
 BACKUP_FOLDER_NAME = "backup"
 SCRIPT_FOLDER_PATH = os.path.dirname(os.path.abspath(__file__))
-
 BACKUP_FOLDER_PATH = os.path.join(SCRIPT_FOLDER_PATH, BACKUP_FOLDER_NAME)
+TODAY_BACKUP_FOLDER_PATH = os.path.join(BACKUP_FOLDER_PATH, datetime.datetime.now().strftime('%Y%m%d'))
+BACKUP_RETENTION_PERIOD_IN_DAYS = 7
 
-if not os.path.exists(BACKUP_FOLDER_PATH):
-    os.makedirs(BACKUP_FOLDER_PATH)
+def create_folders():
+    """
+    Checks if needed folders exist and, if not, creates them
+    """
+    folders = [BACKUP_FOLDER_PATH, TODAY_BACKUP_FOLDER_PATH]
+    for folder in folders:
+        if not os.path.exists(folder):
+            print "Creating folder: " + folder
+            os.makedirs(folder)
+        else:
+            print "Folder already exists: " + folder
 
-github_api_response = requests.get("https://api.github.com/users/" + GITHUB_USERNAME + "/repos").text
-github_api_response = json.loads(github_api_response)
+def get_repository_list_for_specified_user():
+    """
+    Gets complete repository list for specified GITHUB_USERNAME
+    """
+    github_api_response = requests.get("https://api.github.com/users/" + GITHUB_USERNAME + "/repos").text
+    repository_list = json.loads(github_api_response)
+    return repository_list
 
-os.chdir(BACKUP_FOLDER_PATH)
+def perform_backup():
+    """
+    Clones all repositories for specified GITHUB_USERNAME into TODAY_BACKUP_FOLDER_PATH
+    """
+    print "Checking / Creating backup folders..."
+    create_folders()
+    print "Changing current directory to: " + TODAY_BACKUP_FOLDER_PATH
+    os.chdir(TODAY_BACKUP_FOLDER_PATH)
+    print "Running git clone of all available repositories for user: " + GITHUB_USERNAME
+    try:
+        result = [subprocess.call(["git", "clone", item['clone_url']]) for item in get_repository_list_for_specified_user()]
+        print "Backup is done, hoorray! :)"
+    except OSError as e:
+        print "Ouch! Backup threw an error:"
+        print e.strerror
 
-try:
-    result = [subprocess.call(["git", "clone", item['clone_url']]) for item in github_api_response]
-    print "Backup is done, hoorray! :)"
-except OSError as e:
-    print "Ouch! Backup threw an error:"
-    print e.strerror
+def rotate_folders():
+    """
+    Deletes all folders in BACKUP_FOLDER_PATH older than BACKUP_RETENTION_PERIOD_IN_DAYS
+    """
+    backup_folders_list = os.listdir(BACKUP_FOLDER_PATH)
+    sorted_backup_folders_list = sorted(BACKUP_FOLDER_PATH)
+    if len(sorted_backup_folders_list) > BACKUP_RETENTION_PERIOD_IN_DAYS:
+        folder_to_delete = sorted_backup_folders_list.pop()
+        folder_to_delete_path = os.path.join(BACKUP_FOLDER_PATH, folder_to_delete)
+        shutil.rmtree(folder_to_delete_path)
+      
+perform_backup()
+rotate_folders()
